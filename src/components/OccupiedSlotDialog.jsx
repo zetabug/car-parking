5906a97537253a522895df83b56bda3288d37689
Ranvir <ranvirsingh.9693@gmail.com>
2023-09-04 00:56:37 +0530

    Fixes entry time format

--- src/components/OccupiedSlotDialog.jsx
+++ src/components/OccupiedSlotDialog.jsx
@@ -7,36 +7,13 @@ import Button from "@mui/material/Button";
 import { DialogActions } from "@mui/material";
 
 function OccupiedSlotDialog({ open, onClose, slotData, onEndBilling }) {
-  function calcEntrytime(time) {
-    const isoString = time;
 
-    // Create a Date object from the ISO string
-    const date = new Date(isoString);
-
-    // Get hours, minutes, and seconds from the Date object
-    const hours = date.getUTCHours();
-    const minutes = date.getUTCMinutes();
-    const seconds = date.getUTCSeconds();
-
-    // Determine whether it's AM or PM
-    const period = hours >= 12 ? "PM" : "AM";
-
-    // Convert hours to 12-hour format
-    const formattedHours = hours % 12 || 12;
-
-    // Format the time as hh:mm:ss AM/PM
-    const formattedTime = `${String(formattedHours).padStart(2, "0")}:${String(
-      minutes
-    ).padStart(2, "0")}:${String(seconds).padStart(2, "0")} ${period}`;
-
-    return formattedTime; // Output: "02:46:20 PM"
-  }
   return (
     <div>
       <Dialog open={open} onClose={onClose}>
         <DialogContent>
           <DialogTitle>
-            Entry time:{calcEntrytime(slotData.entryTime)}
+            Entry time: {new Date(slotData.entryTime).toLocaleString()}
           </DialogTitle>
         </DialogContent>
         <DialogActions>
